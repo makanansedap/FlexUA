c47388cbc69ddc55877297d84fa78e6594194e5c
vivapti <33304734+vivapti@users.noreply.github.com>
2018-09-15 14:52:56 +0700

    template

--- app/src/main/java/xl/iot/aces/flexua/MainActivity.kt
+++ app/src/main/java/xl/iot/aces/flexua/MainActivity.kt
@@ -1,12 +1,21 @@
 package xl.iot.aces.flexua
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import kotlinx.android.synthetic.main.activity_main.*
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }

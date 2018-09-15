6cd828e32a10413c683afcdb26408ac045716007
vivapti <33304734+vivapti@users.noreply.github.com>
2018-09-15 15:48:48 +0700

    ProgressBar

--- app/src/main/java/xl/iot/aces/flexua/MainActivity.kt
+++ app/src/main/java/xl/iot/aces/flexua/MainActivity.kt
@@ -8,7 +8,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.widget.TextView
-
+import android.app.ProgressDialog
+import android.opengl.Visibility
+import android.os.AsyncTask
+import android.os.Handler
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var progressStatus = 0
+        val handler = Handler()
 
+        startBtn.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            startBtn.isEnabled = false
+            startBtn.text = "Now Analyzing..."
+            Thread(Runnable {
+                while (progressStatus < 100) {
+                    progressStatus += 1
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(Runnable {
+                        progressBar.progress = progressStatus
+                        analyzeTV.text = progressStatus.toString() + "/" + progressBar.max
+                    })
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+                }
+                progressBar.visibility = View.INVISIBLE
+            }).start()
+        }
     }
 }
